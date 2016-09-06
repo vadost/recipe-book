f1d14cdf6292b52332e7d943980604a2fe6c4fb3
Vadym <vadoost@gmail.com>
2016-09-06 19:01:57 +0300

    ready for deployment

---
+++ src/app/recipe-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { RecipeService } from "./recipes";
+
+@Component({
+  moduleId: module.id,
+  selector: 'recipe-book-app',
+  templateUrl: 'recipe-book.component.html'
+})
+export class RecipeBookAppComponent {
+}
